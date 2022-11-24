2158cf996618aa5034652feea6b0e40491aa2e63
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-11-24 05:05:19 +0200

    half step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Field, Form, Formik, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    const schema = yup.object().shape({
+      name: yup
+        .string()
+        .required()
+        .matches(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/),
+    });
+    const initialValues = {
+      name: '',
+    };
+    return (
+      <>
+        <Formik
+          initialValues={initialValues}
+          validate={schema}
+          onSubmit={(values, actions) => {
+            console.log(values, actions);
+          }}
+        >
+          <Form>
+            <Field type="text" name="name" />
+
+            <ErrorMessage name="name" component="div" />
+          </Form>
+        </Formik>
+      </>
+    );
+  }
+}
+
+export default App;
