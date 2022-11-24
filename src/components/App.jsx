9843ab36fb2ee2b22d64cbcd9ac1c95b9fb6db05
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-11-24 14:21:03 +0200

    step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import { Field, Form, Formik, ErrorMessage } from 'formik';
 import * as yup from 'yup';
+import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
@@ -8,31 +9,51 @@ class App extends Component {
     name: '',
   };
 
+  onSubmit = (values, options) => {
+    this.setState(prevState => ({
+      contacts: [{ name: values.name, id: nanoid() }, ...prevState.contacts],
+    }));
+  };
+
   render() {
+    const Error = ({ name }) => {
+      return <ErrorMessage name={name} render={message => <p>{message}</p>} />;
+    };
     const schema = yup.object().shape({
       name: yup
         .string()
         .required()
-        .matches(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/),
+        .matches(
+          /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+          "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        ),
     });
     const initialValues = {
       name: '',
     };
     return (
       <>
+        <h1>Phonebook</h1>
         <Formik
           initialValues={initialValues}
-          validate={schema}
-          onSubmit={(values, actions) => {
-            console.log(values, actions);
-          }}
+          validationSchema={schema}
+          onSubmit={this.onSubmit}
         >
           <Form>
-            <Field type="text" name="name" />
-
-            <ErrorMessage name="name" component="div" />
+            <label>
+              Name
+              <Field type="text" name="name" key={nanoid()} />
+            </label>
+            <Error name="name" />
+            <button type="submit">Add contact</button>
           </Form>
         </Formik>
+        <h2>Contacts</h2>
+        <ul>
+          {this.state.contacts.map(contact => (
+            <li>{contact.name}</li>
+          ))}
+        </ul>
       </>
     );
   }
