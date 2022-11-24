3e377b7f0ffc8bf34629c01247ff01cd2a20d322
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-11-24 14:29:21 +0200

    step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,14 @@ import { nanoid } from 'nanoid';
 class App extends Component {
   state = {
     contacts: [],
-    name: '',
   };
 
   onSubmit = (values, options) => {
     this.setState(prevState => ({
-      contacts: [{ name: values.name, id: nanoid() }, ...prevState.contacts],
+      contacts: [
+        { name: values.name, id: nanoid(), number: values.number },
+        ...prevState.contacts,
+      ],
     }));
   };
 
@@ -27,9 +29,17 @@ class App extends Component {
           /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
           "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         ),
+      number: yup
+        .string()
+        .required()
+        .matches(
+          /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+          'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+        ),
     });
     const initialValues = {
       name: '',
+      number: '+38',
     };
     return (
       <>
@@ -45,13 +55,21 @@ class App extends Component {
               <Field type="text" name="name" key={nanoid()} />
             </label>
             <Error name="name" />
+            <label>
+              Number
+              <Field type="tel" name="number" key={nanoid()} />
+            </label>
+            <Error name="tel" />
             <button type="submit">Add contact</button>
           </Form>
         </Formik>
         <h2>Contacts</h2>
         <ul>
           {this.state.contacts.map(contact => (
-            <li>{contact.name}</li>
+            <li key={nanoid()}>
+              <p>{contact.name}</p>
+              <p>{contact.number}</p>
+            </li>
           ))}
         </ul>
       </>
