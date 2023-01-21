ae70d026003ec02059a53d452c01047d13a89d42
Inna <inna.forkert@gmail.com>
2023-01-21 23:56:12 +0200

    feat: code segmentation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
-import Login from 'pages/Login/Login';
-import MainPage from 'pages/MainPage/MainPage';
-import SignUp from 'pages/SignUp/SignUp';
-import { useEffect } from 'react';
+import { lazy, useEffect } from 'react';
 import { Suspense } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { fetchCurrentUser } from 'redux/utils/createUser';
+
+const MainPage = lazy(() => import('../pages/MainPage'));
+const Login = lazy(() => import('../pages/Login'));
+const SignUp = lazy(() => import('../pages/SignUp'));
 
 const App = () => {
   const dispatch = useDispatch();
